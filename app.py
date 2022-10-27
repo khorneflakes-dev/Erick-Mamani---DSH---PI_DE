5bdace6f50a823b44d77ea5720cba0c5449c0721
khorne flakes <khorneflakesdev@gmail.com>
2022-10-27 16:34:41 -0400

    f

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # definiendo las funciones de limpieza
-# para el dataset que que viene en formato csv
+# para el dataset que viene en formato csv
 def del_dashes(value):
     value = str(value)
     list_value = value.split('-')
@@ -21,15 +21,15 @@ def exctract_unity(value):
 def format_productoId(value):
     if len(str(value)) < 13:
         value_formated = '0'*(15 - len(str(value))) + str(value)
-        return value_formated
+        return value_formated[:-2]
     else:
-        return str(value)
+        return str(value[:-2])
 
 df1 = pd.read_csv('./Datasets/precios_semana_20200413.csv', delimiter=',', encoding='utf-16')
 df2 = pd.read_csv('./Datasets/precios_semana_20200518.txt', delimiter='|')
 df3 = pd.read_json('./Datasets/precios_semana_20200503.json')
-df4 = pd.read_excel('./Datasets/precios_semanas_20200419_20200426.xlsx')
-# df = pd.read_csv('./Datasets/sucursal.csv')
+# df4 = pd.read_excel('./Datasets/precios_semanas_20200419_20200426.xlsx')
+df = pd.read_csv('./Datasets/sucursal.csv')
 df_producto = pd.read_parquet('./Datasets/producto.parquet')
 
 # print(df[df['producto_id']=='9-3-0000000962155'])
@@ -52,11 +52,10 @@ df2 = df2[['producto_id','sucursal_id','precio']]
 df3['producto_id'] = df3['producto_id'].apply(lambda x: del_dashes(x))
 df3 = df3[['producto_id','sucursal_id','precio']]
 
-# limpiando el cuarto dataset de precios
-df4['producto_id'] = df4['producto_id'].astype(int)
-df4['producto_id'] = df4['producto_id'].apply(lambda x: format_productoId(x))
-print(df4.dtypes)
-print(df4)
+# # limpiando el cuarto dataset de precios
+# df4['producto_id'] = df4['producto_id'].astype(str)
+# df4['producto_id'] = df4['producto_id'].apply(lambda x: format_productoId(x))
+# df4 = df4[['producto_id','sucursal_id','precio']]
 
 # limpiando el dataset de productos
 df_producto['presentacion_cantidad'] = df_producto['presentacion'].apply(lambda x: exctract_quantity(x))
@@ -64,3 +63,4 @@ df_producto['presentacion_unidad'] = df_producto['presentacion'].apply(lambda x:
 df_producto['categoria1'] = df_producto['categoria1'].fillna(0)
 df_producto['categoria2'] = df_producto['categoria2'].fillna(0)
 df_producto['categoria3'] = df_producto['categoria3'].fillna(0)
+
